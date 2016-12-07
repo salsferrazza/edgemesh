bd3e1a905748100af247da71ad239dc2f27cbaef
SupremeTechnopriest <randylebeau@gmail.com>
2016-12-07 14:15:51 -0500

    chore(release):  1.3.1

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = function(options) {
+
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+
+    var client = options.client || 'unpkg.com/edgemesh';
+
+    script.type = 'text/javascript';
+    script.onload = function() {
+        window.edgemesh = new window.Edgemesh({ 
+            debug: options.debug || false,
+            host: options.host || 'sig.edgeno.de',
+            client: client,
+            path: options.swPath || '/'
+        });
+    }
+    script.src = 'https://' + client + '/edgemesh.client.min.js';
+    head.appendChild(script);
+
+}
