44dda5f5f06a7ad971041ad914e45f46b2dc1163
SupremeTechnopriest <randylebeau@gmail.com>
2017-01-03 18:18:56 -0500

    ignore tests eslint

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ export default class Edgemesh extends Emitter {
         let script = document.createElement('script');
 
         // Set default client
+        // NOTE: Update this to dist on next push
         let client = opts.client || 'unpkg.com/edgemesh';
 
         // Create script tag
