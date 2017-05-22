0631b2fa853ae447f886d3b929201f38a1e1c411
SupremeTechnopriest <randylebeau@gmail.com>
2017-05-22 15:29:15 -0400

    update sw dist

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -2,7 +2,9 @@
 rimraf dist/edgemesh.client.min.js
 rimraf dist/edgemesh.hub.min.js
 rimraf dist/edgemesh.worker.min.js
+rimraf dist/edgemesh.sw.min.js
 
 cp ../edgemesh-client/dist/edgemesh.client.min.js ./dist
 cp ../edgemesh-hub/dist/edgemesh.hub.min.js ./dist
 cp ../edgemesh-worker/dist/edgemesh.worker.min.js ./dist
+cp ../edgemesh-sw/dist/edgemesh.sw.min.js ./dist
